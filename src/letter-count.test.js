12bb7c9c570cbf9b28e900f9259932873336ee10
juliasut <juliasutygina@gmail.com>
2023-04-11 20:54:22 -0600

    add support for strings with duplicated letters

--- src/letter-count.test.js
+++ src/letter-count.test.js
@@ -5,14 +5,18 @@ describe('getLetterCount - basic functionality', () => {
   it('returns an empty object when passed an empty string', () => {
     const expected = {};
     const actual = getLetterCount('');
-
     expect(actual).to.deep.equal(expected);
   });
 
   it('should return the right letter count for a word with only one of each letter', () => {
     const expected = { c: 1, a: 1, t: 1 };
     const actual = getLetterCount('cat');
+    expect(actual).to.deep.equal(expected);
+  });
 
+  it('should return the correct letter count for words with repetitive letters', () => {
+    const expected = { b: 1, e: 2, t: 2, r: 1 };
+    const actual = getLetterCount('better');
     expect(actual).to.deep.equal(expected);
   });
 });
